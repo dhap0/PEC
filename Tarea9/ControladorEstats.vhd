1ec9b4679de7549a992fe085fb4c241c8bf3fac0
Dhap0 <Dhap0@DESKTOP-RABFCQI>
2021-03-02 22:12:28 +0100

    Entrega 9

--- Tarea9/ControladorEstats.vhd
+++ Tarea9/ControladorEstats.vhd
@@ -8,13 +8,10 @@ ENTITY ControladorEstats IS
 		KEY : IN std_logic_vector(1 DOWNTO 0);
 		fin : IN std_logic;
 		ini : OUT std_logic;
-		rep : OUT std_logic;
-		LEDG: OUT std_logic_vector(7 downto 6)
+		rep : OUT std_logic
 	);
 END ControladorEstats;
 ARCHITECTURE Structure OF ControladorEstats IS 
-signal auxini: std_logic := '0';
-signal auxrep: std_logic := '1';
 
 signal estado    : std_logic := '0';
 signal prxestado : std_logic := '0';
@@ -24,14 +21,6 @@ signal KEY_1 : std_logic := '1';
 
 BEGIN
 
---auxini <= '1' when KEY(1) = '0' and (auxrep = '1' or fin = '1') else '0' when  KEY(0) = '0' or fin = '0';
---auxrep <= '0' when KEY(1) = '0' else '1' when (KEY(0) = '0') and auxrep = '0';
---
---ini <= auxini  when falling_edge(rellotge);
---rep <= auxrep or fin  when falling_edge(rellotge);
-
-LEDG(7) <= prxestado;
-LEDG(6) <= estado;
 estado    <= prxestado when falling_edge(rellotge);
 prxestado <= '1' when estado = '0' and KEY_1 = '0' else
 				 '0' when estado = '1' and (KEY_0 = '0' ) else 
@@ -42,16 +31,13 @@ KEY_0 <= '1' when estado = '0' else
 			
 KEY_1 <= '1' when (estado = '1' and prxestado = '0') or KEY_0 = '0'  else
 			'0' when falling_edge(KEY(1));
-			--'1' when falling_edge(rellotge);
 
 
-auxini <= '1' when estado = '0' and prxestado = '1' else 
+ini <= '1' when estado = '0' and prxestado = '1' else 
 			 '0';
 			 
-auxrep <= '1' when estado = '0' else 
-			 '0';
+rep <= '1' when estado = '0' else 
+			 fin ;
 
-ini <= auxini  when falling_edge(rellotge);
-rep <= auxrep  when falling_edge(rellotge);
 	
 END Structure;
