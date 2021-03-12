f37cd1d4ab24b25dfb5bff8f8f56ef73c8bb6f2f
xXIty <miquel.roset@upc.edu>
2021-03-12 19:07:18 +0100

    implement control_l entity and add necessary constants

--- Etapa1/ficheros_etapa1/control_l.vhd
+++ Etapa1/ficheros_etapa1/control_l.vhd
@@ -2,6 +2,9 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
+use work.sisa16_coop_funct_pkg.all;
+use work.cte_tipos_UF_pkg.all;
+
 ENTITY control_l IS
     PORT (ir     : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
           op     : OUT STD_LOGIC;
@@ -15,7 +18,27 @@ END control_l;
 
 ARCHITECTURE Structure OF control_l IS
 BEGIN
-
-    -- Aqui iria la generacion de las senales de control del datapath
-
+	decoir: process(ir)
+		variable coop:   codigooper;
+		variable funct1: campofunct1;
+	begin
+		
+		coop  := ir(15 downto 15-tamcoop);
+		funct1 := ir(8);
+		ldpc <= haltNO;
+		wrd <= not regPE;
+		
+		case coop is
+			when COOP_MOV =>
+				wrd <= regPE;
+				case funct1 is
+					when MOV_I =>
+						op <= ALU_MOVI;
+					when MOV_HI =>
+						op <= ALU_MOVHI;
+				end case;
+			when COOP_HALT =>
+				ldpc <= haltSI;
+		end case;
+	end process;
 END Structure;
--- Etapa1/ficheros_etapa1/tipos_constantes_pkg/cte_tipos_UF_pkg.vhd
+++ Etapa1/ficheros_etapa1/tipos_constantes_pkg/cte_tipos_UF_pkg.vhd
@@ -3,8 +3,14 @@ use ieee.std_logic_1164.all;
 
 package cte_tipos_UF_pkg is 
 
+constant haltSI: std_logic:= '0';
+constant haltNO: std_logic:= '1';
+
 -- ALU OPCODES
-	constant ALU_MOVI: std_logic := '0';
-	constant ALU_MOVHI: std_logic := '1';
+constant ALU_MOVI: std_logic := '0';
+constant ALU_MOVHI: std_logic := '1';
 -- --
+                                                
+constant regPE:  std_logic:= '1';
+
 END PACKAGE cte_tipos_UF_pkg;
