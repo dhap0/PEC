3218675609a5a21f44e0b812e97ad77c5e9f1d90
xXIty <miquel.roset@upc.edu>
2021-03-16 17:03:20 +0100

    implement sum in alu and add codes in tipos UF

--- Etapa2_1/ficheros_etapa2_1/alu.vhd
+++ Etapa2_1/ficheros_etapa2_1/alu.vhd
@@ -7,7 +7,7 @@ USE work.cte_tipos_UF_pkg.all;
 ENTITY alu IS
     PORT (x  : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
           y  : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
-          op : IN  STD_LOGIC;
+          op : IN  STD_LOGIC_VECTOR(tam_codigo_alu_op-1 downto 0);
           w  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
 END alu;
 
@@ -17,7 +17,9 @@ BEGIN
 
     -- Aqui iria la definicion del comportamiento de la ALU
 	 with op select w <=
-		y 										when ALU_MOVI,
-		y(7 downto 0) & x(7 downto 0) when others;
+		y 										              when ALU_MOVI,
+		y(7 downto 0) & x(7 downto 0)               when ALU_MOVHI,
+		std_logic_vector(unsigned(x) + unsigned(y)) when ALU_SUM,
+		(others => '0') when others;
 
 END Structure;
